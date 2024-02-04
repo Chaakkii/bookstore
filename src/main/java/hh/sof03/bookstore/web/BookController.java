475075c71016d8819d0c96bcc03cffbc2ba7c7f2
Chaakkii <tsangchakfung@gmail.com>
2024-02-04 14:21:22 +0200

    controller muutos

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,12 +1,17 @@
 package hh.sof03.bookstore.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 
 public class BookController {
 
+
     @GetMapping("/index")
     public String getIndex() {
-        return "index";
+        return "tervetulosivu";
     }
 
 }
